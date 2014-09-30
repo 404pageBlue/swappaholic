554a16f02787e1aec2893c9c04d9dad5d2e78901
bkadowaki <brett.kadowaki@gmail.com>
2014-09-30 16:50:06 -0700

    adding link_to's to both the items and users show view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140930023252) do
+ActiveRecord::Schema.define(version: 20140930233240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 20140930023252) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
     t.string   "password"
@@ -35,6 +34,7 @@ ActiveRecord::Schema.define(version: 20140930023252) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "location"
+    t.string   "first_name"
   end
 
 end
