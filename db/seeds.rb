93782b0f7259c7d8e50ac6ac3cb084a5eb100111
bkadowaki <brett.kadowaki@gmail.com>
2014-09-29 18:13:42 -0700

    minified bootstrap theme, styling, and data modeling

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,5 +25,54 @@ items = Item.create([
 		condition: "descent",
 		est_value: 20,
 		willing_to_trade_for: "electronics"
+	},
+	{
+		name: "iPad",
+		description: "Pad of i",
+		condition: "like new",
+		est_value: 200,
+		willing_to_trade_for: "furniture"
+	},
+	{
+		name: "Brandon",
+		description: "Worthless financial analyst",
+		condition: "old and beat up",
+		est_value: 2,
+		willing_to_trade_for: "food"
+	},	
+	{
+		name: "Basketball hoop",
+		description: "Hoop for basketball",
+		condition: "brand new",
+		est_value: 150,
+		willing_to_trade_for: "tv's"
+	},	
+	{
+		name: "Ray Ban sunglasses",
+		description: "Polarized sunglasses",
+		condition: "like new",
+		est_value: 180,
+		willing_to_trade_for: "High heels"
+	},	
+	{
+		name: "Lazy Boy",
+		description: "Single seater lazy boy sofa chair",
+		condition: "heavily used",
+		est_value: 75,
+		willing_to_trade_for: "Game consoles"
+	},	
+	{
+		name: "Evander Hollyfield's ear",
+		description: "cartilidge",
+		condition: "old",
+		est_value: 75000,
+		willing_to_trade_for: "Mike Tyson poster"
+	},	
+	{
+		name: "Long Board",
+		description: "8.5 ft Becker long board",
+		condition: "used",
+		est_value: 500,
+		willing_to_trade_for: "Jewelry"
 	}
 ]);
