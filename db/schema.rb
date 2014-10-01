92cbdbfcfb8f39ad751ae3b645b166571d86a7ca
JustinJitti <JustinJitti@gmail.com>
2014-09-30 17:00:14 -0700

    Final Base

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140930233240) do
+ActiveRecord::Schema.define(version: 20140930235642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,14 +27,12 @@ ActiveRecord::Schema.define(version: 20140930233240) do
   end
 
   create_table "users", force: true do |t|
+    t.string   "first_name"
     t.string   "last_name"
-    t.string   "email"
-    t.string   "password"
     t.integer  "rating"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "location"
-    t.string   "first_name"
   end
 
 end
