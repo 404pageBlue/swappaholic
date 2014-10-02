5b9d06d5dbd3243ecc920d9bbdf827ed2336d558
bkadowaki <brett.kadowaki@gmail.com>
2014-10-02 11:05:52 -0700

    fixing everything

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,11 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
-  end
+  end  
+
+  # def destroy
+  # 	@user = User.find(params[:id])
+  # 	@user.destroy
+  # 	redirect_to root_path
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   root 'welcome#index'
 
+  get '/users/:id', to: 'users#show', as: :user_profile
 
-  resources :users, :items
+  resources :items
 
   # You can have the root of your site routed with "root"
 
