a51034bfee19c8de7926f0c0d85e84e689f3dcf7
bkadowaki <brett.kadowaki@gmail.com>
2014-10-03 16:06:56 -0700

    adding #create #new item, auto-assigning it to current_user and displaying in _marketplace partial

--- Gemfile
+++ Gemfile
@@ -41,3 +41,5 @@ gem 'spring',        group: :development
 gem 'bootstrap-sass', '~> 3.2.0'
 
 gem 'devise'
+
+gem 'pry-rails', :group => :development
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,21 @@ class ItemsController < ApplicationController
   def show
   	@item = Item.find(params[:id])
   end
+
+  def new
+  	@item = Item.new
+  end
+
+  def create
+  	item_params = params.require(:item).permit(:name, :description, :condition, :est_value, :willing_to_trade_form)
+  	item_params[:user_id] = current_user.id
+  	@item = Item.create(item_params)
+  	redirect_to user_profile_path(current_user)
+  end
+
 end
+
+
+
+
+
