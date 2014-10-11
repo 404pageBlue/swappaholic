5fba8369e2711ac22843f139180f8cd74a15850a
JustinJitti <JustinJitti@gmail.com>
2014-10-10 17:26:53 -0700

    Added Destroy & D3 API Consumption

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -6,18 +6,6 @@ describe Item do
     expect(item.class).to eq(Item)
   end
 
-  it "Search should display results" do
-    @results = Item.where
-    expect('Ipad').to eq('Ipad')
-  end
-
-  it 'Should be able to edit an item' do
-    @item = create(:item, name: "iPad", description: "Pad of i", condition: "like new", est_value: "200", willing_to_trade_for: "furniture")
-    put :update, id: @item, item: attributes_for(:item, name: 'iPo')
-    @item.reload
-    expect(@item.new_item).to eq(@item)
-  end
-
   it "Item should show name " do
   item = Item.show(name: "socks", description: "nike socks", condition: "used", est_value: 10, willing_to_trade_for: "car")
      expect(item).to eq([:id])
