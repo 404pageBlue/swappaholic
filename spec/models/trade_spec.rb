b736f733302b59a1984fe04142de4a45f0ac825f
JustinJitti <JustinJitti@gmail.com>
2014-10-12 14:05:40 -0700

    11 tests Completed

--- spec/models/trade_spec.rb
+++ spec/models/trade_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Trade, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Trade do
+  it '#new' do
+    @item = Item.new
+    expect(@item).to eq(@item)
+  end
+
+  it '#new' do
+    @item = Item.new
+    expect(@item).to be_instance_of(Item)
+  end
 end
