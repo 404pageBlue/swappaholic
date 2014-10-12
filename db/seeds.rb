8d76e08cc3043f10c88d9e7479897a797805d1fc
Brett Kadowaki <brett.kadowaki@gmail.com>
2014-10-11 18:21:19 -0700

    cleaning up seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,21 +15,21 @@ users = User.create!([
 		password: "12345678",
 		password_confirmation: "12345678"
 		#encrypted_password: '#$taawktljasktlw4aaglj'
-	},
-	{
-		firstname: "Justin",
-		lastname: "Jitti",
-		email: "justin@gmail.com",
-		password: "12345678",
-		password_confirmation: "12345678"
-	}, 
-	{
-		firstname: "Kevin",
-		lastname: "Backend",
-		email: "kevin@gmail.com",
-		password: "12345678",
-		password_confirmation: "12345678"
 	}
+	# {
+	# 	firstname: "Justin",
+	# 	lastname: "Jitti",
+	# 	email: "justin@gmail.com",
+	# 	password: "12345678",
+	# 	password_confirmation: "12345678"
+	# }, 
+	# {
+	# 	firstname: "Kevin",
+	# 	lastname: "Backend",
+	# 	email: "kevin@gmail.com",
+	# 	password: "12345678",
+	# 	password_confirmation: "12345678"
+	# }
 	]);
 
 Item.destroy_all
@@ -41,64 +41,64 @@ items = Item.create([
 		est_value: 20,
 		willing_to_trade_for: "electronics",
 		user_id: 1
-	},
-	{
-		name: "iPad",
-		description: "Pad of i",
-		condition: "like new",
-		est_value: 200,
-		willing_to_trade_for: "furniture",
-		user_id: 1
-	},
-	{
-		name: "Brandon",
-		description: "Worthless financial analyst",
-		condition: "old and beat up",
-		est_value: 2,
-		willing_to_trade_for: "food",
-		user_id: 2
-	},	
-	{
-		name: "Basketball hoop",
-		description: "Hoop for basketball",
-		condition: "brand new",
-		est_value: 150,
-		willing_to_trade_for: "tv's",
-		user_id: 2
-	},	
-	{
-		name: "Ray Ban sunglasses",
-		description: "Polarized sunglasses",
-		condition: "like new",
-		est_value: 180,
-		willing_to_trade_for: "High heels",
-		user_id: 3
-	},	
-	{
-		name: "Lazy Boy",
-		description: "Single seater lazy boy sofa chair",
-		condition: "heavily used",
-		est_value: 75,
-		willing_to_trade_for: "Game consoles",
-		user_id: 3
-	},	
-	{
-		name: "Evander Hollyfield's ear",
-		description: "cartilidge",
-		condition: "old",
-		est_value: 75000,
-		willing_to_trade_for: "Mike Tyson 
-		poster",
-		user_id: 2	
-	},	
-	{
-		name: "Long Board",
-		description: "8.5 ft Becker long board",
-		condition: "used",
-		est_value: 500,
-		willing_to_trade_for: "Jewelry",
-		user_id: 3		
 	}
+	# {
+	# 	name: "iPad",
+	# 	description: "Pad of i",
+	# 	condition: "like new",
+	# 	est_value: 200,
+	# 	willing_to_trade_for: "furniture",
+	# 	user_id: 1
+	# },
+	# {
+	# 	name: "Brandon",
+	# 	description: "Worthless financial analyst",
+	# 	condition: "old and beat up",
+	# 	est_value: 2,
+	# 	willing_to_trade_for: "food",
+	# 	user_id: 2
+	# },	
+	# {
+	# 	name: "Basketball hoop",
+	# 	description: "Hoop for basketball",
+	# 	condition: "brand new",
+	# 	est_value: 150,
+	# 	willing_to_trade_for: "tv's",
+	# 	user_id: 2
+	# },	
+	# {
+	# 	name: "Ray Ban sunglasses",
+	# 	description: "Polarized sunglasses",
+	# 	condition: "like new",
+	# 	est_value: 180,
+	# 	willing_to_trade_for: "High heels",
+	# 	user_id: 3
+	# },	
+	# {
+	# 	name: "Lazy Boy",
+	# 	description: "Single seater lazy boy sofa chair",
+	# 	condition: "heavily used",
+	# 	est_value: 75,
+	# 	willing_to_trade_for: "Game consoles",
+	# 	user_id: 3
+	# },	
+	# {
+	# 	name: "Evander Hollyfield's ear",
+	# 	description: "cartilidge",
+	# 	condition: "old",
+	# 	est_value: 75000,
+	# 	willing_to_trade_for: "Mike Tyson 
+	# 	poster",
+	# 	user_id: 2	
+	# },	
+	# {
+	# 	name: "Long Board",
+	# 	description: "8.5 ft Becker long board",
+	# 	condition: "used",
+	# 	est_value: 500,
+	# 	willing_to_trade_for: "Jewelry",
+	# 	user_id: 3		
+	# }
 ]);
 
 Trade.destroy_all
